23ca430f8aed42ee2cc22a7ded281577cb12cf5e
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2022-03-11 03:01:13 +0900

    github

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "../root/style";
 import { Body } from "../components/Body";
 import { Navbar } from "../components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export const Root = () => {
   return (
