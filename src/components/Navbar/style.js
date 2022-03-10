8b2d9c168aad7e5b72a8d375a31fb9a4c3e89a12
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2022-03-11 06:30:00 +0900

    update

--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-
 export const Container = styled.div`
   border: 1px solid red;
   width: 100%;
   height: 700px;
+  background: #e5e5e5;
 `;
 
 export const Dashbar = styled.div`
@@ -134,6 +134,7 @@ export const Shopbarprice1 = styled.div`
   flex: 1;
   border: 1px solid red;
   height: 400px;
+  display: flex;
   /* width: 800px; */
 `;
 export const Shopbarprice2 = styled.div`
@@ -141,4 +142,236 @@ export const Shopbarprice2 = styled.div`
   border: 1px solid red;
   height: 400px;
   /* width: 800px; */
+`;
+export const Shopflower = styled.div`
+  /* flex-direction: column; */
+  /* flex-direction: column-reverse; */
+`;
+export const Flowermap = styled.img`
+  flex-direction: column;
+  display: block;
+`;
+export const Shopflower1 = styled.img`
+  width: 360px;
+  height: 360px;
+  margin-left: 55px;
+`;
+
+export const Daisy = styled.h1`
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 28px;
+  line-height: 16px;
+  color: #3d3d3d;
+  margin-top: 8px;
+`;
+export const Dollar = styled.div`
+  display: flex;
+  border-bottom: 0.3px solid rgba(70, 163, 88, 0.5);
+  align-items: center;
+  justify-content: space-between;
+  margin-top: -15px;
+`;
+
+export const Money = styled.div`
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 22px;
+  line-height: 16px;
+  color: #46a358;
+`;
+export const Stardiv = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const Star = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+export const Customer = styled.p`
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 16px;
+  color: #3d3d3d;
+`;
+export const Short = styled.p`
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 16px;
+  color: #3d3d3d;
+  margin-top: 8px;
+`;
+
+export const Text = styled.p`
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+  color: #727272;
+  margin-top: -8px;
+`;
+export const Size = styled.p`
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 16px;
+  color: #3d3d3d;
+  margin-top: -8px;
+`;
+export const Sizes = styled.div`
+  display: flex;
+  margin-top: -8px;
+`;
+export const Types = styled.p`
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+
+  text-align: center;
+
+  color: #727272;
+  margin-left: 10px;
+  margin-right: 10px;
+  border: 1px solid black;
+  border-radius: 15px;
+  width: 17px;
+  height: 17px;
+`;
+export const Card = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const Minus = styled.div`
+  background: linear-gradient(180deg, #46a358 0%, rgba(70, 163, 88, 0.8) 100%);
+  border-radius: 29px;
+  color: white;
+  width: 23px;
+  height: 30px;
+  text-align: center;
+  align-items: center;
+  margin-left: 7px;
+  margin-right: 7px;
+`;
+export const Num = styled.p`
+  color: black;
+  margin-left: 7px;
+  margin-right: 7px;
+`;
+export const Plus = styled.div`
+  background: linear-gradient(180deg, #46a358 0%, rgba(70, 163, 88, 0.8) 100%);
+  border-radius: 29px;
+  color: white;
+  width: 23px;
+  height: 30px;
+  text-align: center;
+  align-items: center;
+  margin-left: 7px;
+  margin-right: 7px;
+`;
+export const Buy = styled.div`
+  background: #46a358;
+  border-radius: 6px;
+  color: white;
+  width: 120px;
+  height: 30px;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  margin-left: 7px;
+  margin-right: 7px;
+  line-height: 20px;
+  /* identical to box height, or 143% */
+
+  text-transform: uppercase;
+`;
+export const Add = styled.div`
+  /* background: #46a358; */
+  border: 1px solid #46a358;
+  border-radius: 6px;
+  color: #46a358;
+  width: 120px;
+  height: 30px;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 700;
+  margin-left: 7px;
+  margin-right: 7px;
+  font-size: 14px;
+  line-height: 20px;
+  /* identical to box height, or 143% */
+
+  text-transform: uppercase;
+
+  color: #46a358;
+`;
+
+export const Heart = styled.img`
+  border: 1px solid #46a358;
+  box-sizing: border-box;
+  margin-left: 7px;
+  margin-right: 7px;
+  border-radius: 6px;
+  width: 33px;
+  height: 30px;
+`;
+
+export const Tags = styled.p`
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 16px;
+  color: #a5a5a5;
+  margin-top: -8px;
+`;
+export const Tags1 = styled.p`
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 16px;
+  color: #a5a5a5;
+  margin-top: -8px;
+`;
+export const Tags2 = styled.p`
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 16px;
+  color: #a5a5a5;
+  margin-top: -8px;
+`;
+export const Insta = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: -10px;
+`;
+export const Product = styled.p`
+  font-family: "Cera Pro";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 15px;
+  line-height: 16px;
+  color: #3d3d3d;
+`;
+export const Internet = styled.img`
+  margin: 15px 18px;
 `;
--- src/components/mock/index.js
+++ src/components/mock/index.js
@@ -1,10 +1,9 @@
-import React from "react";
 import flower from "../assets/images/flower.png";
 
 export const flowers = [
-  { id: 1, item: { flower } },
-  { id: 2, item: { flower } },
-  { id: 3, item: { flower } },
-  { id: 4, item: { flower } },
+  { id: 1, item: flower },
+  { id: 2, item: flower },
+  { id: 3, item: flower },
+  { id: 4, item: flower },
 ];
 export default flowers;
